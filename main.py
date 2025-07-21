4fa066a802554cf444e27ccddbfc71e4d520a364
Henry Tian <chariothy@gmail.com>
2025-07-21 23:29:34 +0800

    Add hs300 in the batch job.

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
             #macro.sentiment(slient=True), ## 情绪指数从2025-06-08开始出现json解析错误，可能是返回了空字符串
             #macro.index(slient=True), ## 指数估值从1.15.51开始被删除
             #macro.sh300_fear_greed(slient=True), ## 指数估值从1.15.65开始被删除
+            macro.sh300_index(slient=True),
         ]
         valid_results = [r for r in results if r is not None]
         fin.debug(valid_results)
